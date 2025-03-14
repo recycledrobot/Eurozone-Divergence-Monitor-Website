040e7f1c9d9aeaea38fddd9ce5e325712246dce8
impshum <impshum@recycledrobot.co.uk>
2025-03-14 11:49:00 +0000

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    "title": "Divergence Monitor",
-    "description": "Live divergence monitor for market analysis.",
-    "keywords": "divergence, market analysis, trading, live data",
-    "author": "Divergence Monitor",
-    "url": "https://yourwebsite.com/",
-    "image": "https://yourwebsite.com/preview-image.jpg",
-    "iframeSrc": "https://divergencemonitor.streamlit.app/?embed=true"
-};
